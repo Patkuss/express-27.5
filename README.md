3da97c6bf1261b737d5965b07bf454957211a4e7
Patryk Płaczek <patryk.placzek@wp.pl>
2020-03-19 12:44:48 +0100

    Add task 27.5

---
+++ README.md
@@ -0,0 +1,17 @@
+GET /testimonials – zwracanie całej listy wpisów.
+GET /testimonials/:id – zwracanie konkretnego wpisu.
+GET /testimonials/random – zwracanie losowego wpisu.
+POST /testimonials – dodawanie nowego wpisu na bazie req.body, otrzymanego od części frontendowej.
+PUT /testimonials/:id – modyfikacja wpisu o danym id na bazie req.body otrzymanego od części frontendowej.
+DELETE /testimonials/:id – usunięcie wpisu o konkretnym id.
+
+GET /concerts
+GET /concerts/:id
+POST /concerts
+DELETE /concerts/:id
+PUT /concerts/:id
+GET /seats
+GET /seats/:id
+POST /seats
+DELETE /seats/:id
+PUT /seats/id
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const testimonialsRoutes = require('./routes/testimonials.routes');
+const concertsRoutes = require('./routes/concerts.routes');
+const seatsRoutes = require('./routes/seats.routes');
+
+app.use(cors());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use('/api', testimonialsRoutes);
+app.use('/api', concertsRoutes);
+app.use('/api', seatsRoutes);
+
+app.get('/', (req, res) => {
+  res.send('home');
+});
+
+app.use((req, res) => {
+  res.status(404).send({ message: 'Not found...' });
+});
+
+app.listen(8000, () => {
+  console.log('Server is running on port: 8000');
+});
